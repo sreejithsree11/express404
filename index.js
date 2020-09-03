e5a315c137d89adee121b58d36da864bdcc5be03
Sreejith M S <sreejithms@Sreejiths-MacBook-Air.local>
2020-09-03 15:13:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Functionto handle the 404 condition.
+ * @param {Object} app 
+ * @param {string} type 
+ * @param {string} value 
+ */
+function express404(app, type, value = '404') {
+    if (type === 'template') {
+        app.set('view engine', 'ejs');
+        // Assume 404 since no middleware responded
+        app.use(function (req, res) {
+            res.render(value);
+        });
+    } else {
+        // Assume 404 since no middleware responded
+        app.use(function (req, res) {
+            res.status(404).json({
+                statusCode: 404,
+                message: value
+            });
+        });
+    }
+}
+
+module.exports = express404;
